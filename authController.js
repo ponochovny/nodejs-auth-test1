b626f26cb440ba959765434879467cc99e242ef8
ponochovny <ponochovny@gmail.com>
2022-10-24 10:05:18 +0300

    jwt token setup

--- authController.js
+++ authController.js
@@ -1,7 +1,17 @@
 const User = require('./models/User')
 const Role = require('./models/Role')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const { validationResult } = require('express-validator')
+const { secret } = require('./config')
+
+const generateAccessToken = (id, roles) => {
+	const payload = {
+		id,
+		roles,
+	}
+	return jwt.sign(payload, secret, { expiresIn: '24h' })
+}
 
 class authController {
 	async registration(req, res) {
@@ -34,6 +44,19 @@ class authController {
 	}
 	async login(req, res) {
 		try {
+			const { username, password } = req.body
+			const user = await User.findOne({ username })
+			if (!user) {
+				return res
+					.status(400)
+					.json({ message: `User ${username} is not found` })
+			}
+			const validPassword = bcrypt.compareSync(password, user.password)
+			if (!validPassword) {
+				return res.status(400).json({ message: 'Entered wrong password' })
+			}
+			const token = generateAccessToken(user._id, user.roles)
+			return res.json({ token })
 		} catch (error) {
 			console.log(error)
 			res.status(400).json({ message: 'Login error' })
@@ -41,7 +64,8 @@ class authController {
 	}
 	async getUsers(req, res) {
 		try {
-			res.json('server works')
+			const users = await User.find()
+			res.json(users)
 		} catch (error) {
 			console.log(error)
 		}
--- authRouter.js
+++ authRouter.js
@@ -2,6 +2,7 @@ const Router = require('express')
 const router = new Router()
 const controller = require('./authController')
 const { check } = require('express-validator')
+const authMiddleware = require('./middleware/authMiddleware')
 
 router.post(
 	'/registration',
@@ -15,6 +16,6 @@ router.post(
 	controller.registration
 )
 router.post('/login', controller.login)
-router.get('/users', controller.getUsers)
+router.get('/users', authMiddleware, controller.getUsers)
 
 module.exports = router
