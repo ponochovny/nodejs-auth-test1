e8422132897898605b313dae7ffb6c52a3d547ba
ponochovny <ponochovny@gmail.com>
2022-10-24 08:16:32 +0300

    connect mongodb

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express')
+const { default: mongoose } = require('mongoose')
 const PORT = process.env.PORT || 5000
 
 const app = express()
 
-const start = () => {
+app.use(express.json())
+
+const start = async () => {
 	try {
+		await mongoose.connect(
+			`mongodb+srv://admin:qwerty123@cluster0.jnzusts.mongodb.net/auth_roles?retryWrites=true&w=majority`
+		)
 		app.listen(PORT, () => console.log(`server started on port ${PORT}`))
 	} catch (error) {
 		console.log(error)
